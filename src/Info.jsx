ea7fd09408ac8d81c3c712004c99c7394adc95fa
Saklain Lingkon <100770641+SLingkon@users.noreply.github.com>
2022-08-21 04:52:52 +0600

    Update Info.jsx

--- src/Info.jsx
+++ src/Info.jsx
@@ -7,7 +7,7 @@ function Info() {
 				<h1>Md Maruf Saklain lingkon</h1>
 				<h2>Web Enthusiast</h2>
 				<p>
-					"I love to write clean code and follow the best practices. Curious
+					"Hi,I love to write clean code and follow the best practices. Curious
 					about designing and finding unique ideas for developments. Seeking a
 					position to become a reliable software engineer where I will able to
 					learn and apply core software engineering architectures and system
